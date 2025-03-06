ac02dcee98fe2ef6f783ba14579ed6a7f8cb7067
lemon <git@unseen.ninja>
2025-03-06 12:05:34 +0100

    chore(vs-code): update config to new format

--- modules/home-manager/vs-code.nix
+++ modules/home-manager/vs-code.nix
@@ -1,47 +1,52 @@
 { pkgs, ... }: { programs.vscode = {
 
   enable = true;
-  enableUpdateCheck = false;
-  enableExtensionUpdateCheck = false;
-  mutableExtensionsDir = false;
+  mutableExtensionsDir = true;
+
+  profiles.default = {
+
+    enableUpdateCheck = false;
+    enableExtensionUpdateCheck = false;
+
+    extensions = with pkgs.vscode-extensions; [
+      astro-build.astro-vscode
+      bbenoist.nix
+      bradlc.vscode-tailwindcss
+      catppuccin.catppuccin-vsc
+      catppuccin.catppuccin-vsc-icons
+      eamodio.gitlens
+      editorconfig.editorconfig
+      vue.volar
+    ];
+
+    userSettings = {
+      "telemetry.telemetryLevel" = "on";
+
+      # Workbench Settings
+      "workbench.statusBar.visible" = false;
+      "editor.minimap.enabled" = false;
+      "workbench.activityBar.location" = "hidden";
+
+      # Theme Settings
+      "workbench.colorTheme" = "Catppuccin Frappé";
+      "workbench.iconTheme" = "catppuccin-frappe";
+      "catppuccin.workbenchMode" = "flat";
+
+      # Editor Settings
+      "editor.fontSize" = 12;
+      "editor.fontFamily" = "'Fira Code', 'monospace', monospace";
+      "editor.fontLigatures" = true;
+      "editor.renderWhitespace" = "all";
+      "editor.tabSize" = 2;
+
+      # File Settings
+      "files.trimTrailingWhitespace" = true;
+      "files.trimFinalNewlines" = true;
+      "files.insertFinalNewline" = true;
+      "diffEditor.ignoreTrimWhitespace" = false;
+    };
 
 
-  extensions = with pkgs.vscode-extensions; [
-    astro-build.astro-vscode
-    bbenoist.nix
-    catppuccin.catppuccin-vsc
-    catppuccin.catppuccin-vsc-icons
-    eamodio.gitlens
-    editorconfig.editorconfig
-    vue.volar
-  ];
-
-
-  userSettings = {
-    "telemetry.telemetryLevel" = "off";
-
-    # Workbench Settings
-    "workbench.statusBar.visible" = false;
-    "editor.minimap.enabled" = false;
-    "workbench.activityBar.location" = "hidden";
-
-    # Theme Settings
-    "workbench.colorTheme" = "Catppuccin Frappé";
-    "workbench.iconTheme" = "catppuccin-frappe";
-    "catppuccin.workbenchMode" = "flat";
-
-    # Editor Settings
-    "editor.fontSize" = 12;
-    "editor.fontFamily" = "'Fira Code', 'monospace', monospace";
-    "editor.fontLigatures" = true;
-    "editor.renderWhitespace" = "all";
-    "editor.tabSize" = 2;
-
-    # File Settings
-    "files.trimTrailingWhitespace" = true;
-    "files.trimFinalNewlines" = true;
-    "files.insertFinalNewline" = true;
-    "diffEditor.ignoreTrimWhitespace" = false;
   };
 
 };}
