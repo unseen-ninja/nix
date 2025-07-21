8b637358782f2bc3d64ef3c448e601f7cba31492
lemon <git@unseen.ninja>
2025-07-21 18:36:15 +0200

    chore(vs-code): update settings

--- modules/home-manager/vs-code.nix
+++ modules/home-manager/vs-code.nix
@@ -22,36 +22,52 @@
     userSettings = {
       "telemetry.telemetryLevel" = "on";
 
-      # Workbench Settings
-      "workbench.statusBar.visible" = false;
-      "editor.minimap.enabled" = false;
-      "workbench.activityBar.location" = "hidden";
-
-      # Theme Settings
+      # Theme Settings (Catppuccin)
       "workbench.colorTheme" = "Catppuccin Frappé";
       "workbench.iconTheme" = "catppuccin-frappe";
       "catppuccin.workbenchMode" = "flat";
 
+      # General
+      "breadcrumbs.enabled" = false;
+      "chat.commandCenter.enabled" = false;
+      "window.commandCenter" = false;
+      "workbench.layoutControl.enabled" = false;
+
+      # Workbench Settings
+      "workbench.activityBar.location" = "hidden";
+      "workbench.navigationControl.enabled" = false;
+      "workbench.sideBar.location" = "left";
+      "workbench.startupEditor" = "none";
+      "workbench.statusBar.visible" = false;
+      "workbench.tips.enabled" = false;
+      "workbench.tree.enableStickyScroll" = false;
+      "workbench.tree.renderIndentGuides" = "none";
+      "workbench.tree.indent" = 12;
+
       # Editor Settings
-      "editor.fontSize" = 12;
       "editor.fontFamily" = "'Fira Code', 'monospace', monospace";
       "editor.fontLigatures" = true;
+      "editor.fontSize" = 12;
+      "editor.lineHeight" = 2;
+      "editor.minimap.enabled" = false;
       "editor.renderWhitespace" = "all";
+      "editor.stickyScroll.enabled" = false;
       "editor.tabSize" = 2;
 
+      # Explorer Settings
+      "explorer.compactFolders" = false;
+      "explorer.confirmDelete" = false;
+      "explorer.confirmDragAndDrop" = false;
+
       # File Settings
-      "files.trimTrailingWhitespace" = true;
-      "files.trimFinalNewlines" = true;
-      "files.insertFinalNewline" = true;
       "diffEditor.ignoreTrimWhitespace" = false;
+      "files.insertFinalNewline" = true;
+      "files.trimFinalNewlines" = true;
+      "files.trimTrailingWhitespace" = true;
 
       "files.associations" = {
         "*.svg" = "default";
       };
-
     };
-
-
   };
-
 };}
